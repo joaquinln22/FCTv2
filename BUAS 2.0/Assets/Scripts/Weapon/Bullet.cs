9a4a0aa0ea61e4370b63e594e11341431bd1b2ac
Joaquin Lorca Nieto <joaquinlorcanieto@gmail.com>
2025-08-30 18:04:50 +0200

    Creando mapa Whisky Peak
    Parte 1

--- BUAS 2.0/Assets/Scripts/Weapon/Bullet.cs
+++ BUAS 2.0/Assets/Scripts/Weapon/Bullet.cs
@@ -8,7 +8,11 @@ public class Bullet : MonoBehaviour
     {
         if (collision.gameObject.CompareTag("Enemy"))
         {
-            collision.gameObject.GetComponent<AI>().LooseLife(1);  
+            collision.gameObject.GetComponent<AI>().LooseLife(1);
+        }
+        else if (collision.gameObject.CompareTag("Diana"))
+        {
+            Destroy(collision.gameObject); // Destruye la diana
         }
     }
 }
