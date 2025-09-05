fa5009ff9a16ba1728b65ca737a0e776919e58e1
Joaquin Lorca Nieto <joaquinlorcanieto@gmail.com>
2025-09-05 03:18:01 +0200

    Menu inicial del juego
    Parte 2

---
+++ BUAS 2.0/Assets/Scripts/InitialMenu/OptionsMenu.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class OptionsMenu : MonoBehaviour
+{
+    [SerializeField] private AudioMixer audioMixer;
+
+    public void FullScreen(bool pantallaCompleta)
+    {
+        Screen.fullScreen = pantallaCompleta;
+    }
+
+    public void ChangeVolume(float volumen)
+    {
+        audioMixer.SetFloat("Volume", volumen);
+    }
+
+    public void ChangeResolution()
+    {
+        
+    }
+}
